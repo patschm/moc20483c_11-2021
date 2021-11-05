23a001e4b660aec8da056811158a6e1e042184ec
Patrick Schmidt <pschmidt@pasit.nl>
2021-11-05 15:41:35 +0100

    day5

---
+++ Module_9/Calculator/CalculatorViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace Calculator
+{
+    public class RelayCommand : ICommand
+    {
+        #region Fields 
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+        #endregion // Fields 
+        #region Constructors 
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _execute = execute; _canExecute = canExecute;
+        }
+        #endregion // Constructors 
+        #region ICommand Members 
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+        public void Execute(object parameter) { _execute(parameter); }
+        #endregion // ICommand Members 
+    }
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private int a;
+        private int b;
+        private int result;
+
+        public int Result
+        {
+            get { return result; }
+            set 
+            { 
+                result = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Result)));
+            }
+        }
+
+        public int B
+        {
+            get { return b; }
+            set 
+            { 
+                b = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(B)));
+            }
+        }
+        public int A
+        {
+            get { return a; }
+            set 
+            { 
+                a = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(A)));
+            }
+        }
+
+        private RelayCommand relayAddCommand;
+
+        public ICommand MyProperty
+        {
+            get 
+            {
+                if (relayAddCommand == null)
+                {
+                    relayAddCommand = new RelayCommand(par => Result = A + B , null);
+                }
+                return relayAddCommand;
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
